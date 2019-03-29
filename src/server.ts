38b70e78ff2a4d5539c261ec57df66fa870e0112
Nicolás Santángelo <santangelonicolas@gmail.com>
2019-03-29 11:55:16 +0100

    chore: reorder code

--- src/server.ts
+++ src/server.ts
@@ -4,21 +4,20 @@ import { ExpressApp } from './common/ExpressApp'
 
 const SERVER_PORT = env.get('SERVER_PORT', '5000')
 const API_VERSION = env.get('API_VERSION', 'v1')
-
-const app = new ExpressApp()
-
-const corsOrigin = env.isDevelopment() ? '*' : env.get('CORS_ORIGIN', '')
-const corsMethod = env.isDevelopment() ? '*' : env.get('CORS_METHOD', '')
+const CORS_ORIGIN = env.get('CORS_ORIGIN', '*')
+const CORS_METHOD = env.get('CORS_METHOD', '*')
 
 const auth = {
   username: env.get('AUTH_USERNAME', ''),
   password: env.get('AUTH_PASSWORD', '')
 }
 
+const app = new ExpressApp()
+
 app
   .useJSON()
   .useVersion(API_VERSION)
-  .useCORS(corsOrigin, corsMethod)
+  .useCORS(CORS_ORIGIN, CORS_METHOD)
 
 // Mount routers
 new ContestRouter(app).mount()

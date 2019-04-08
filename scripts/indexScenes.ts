361eae41f2cd106ba53bf9dd815831dda497a568
Ariel Barmat <abarmat@decentraland.org>
2019-04-08 07:28:23 +0200

    refactor: update based on code review

--- scripts/indexScenes.ts
+++ scripts/indexScenes.ts
@@ -1,28 +1,34 @@
 import * as fs from 'fs'
 import * as path from 'path'
 
-import * as crypto from '../src/crypto'
 import { Log, env } from 'decentraland-commons'
 import { db, Model } from 'decentraland-server'
 
+import * as crypto from '../src/crypto'
+import { BaseEntry } from '../src/Contest/types'
+
 const log = new Log('IndexScenes')
 
-export interface OptionAttributes {
+export interface SceneAttributes {
   id: string
-  value: JSON
+  value: BaseEntry
   email: string
   title: string
   description?: string
   address?: string
-  parcels_count: Number
-  triangles_count: Number
-  items_count: Number
-  env: string
-  created_at: Date
-  updated_at: Date
+  parcels_count: number
+  triangles_count: number
+  items_count: number
+  sessions: number
+  days: number
+  n_events: number
+  create_time: number
+  tag: string
+  created_at?: Date
+  updated_at?: Date
 }
 
-class Scene extends Model<OptionAttributes> {
+class Scene extends Model<SceneAttributes> {
   static tableName = 'scenes'
 }
 
@@ -30,21 +36,21 @@ const getSceneList = (baseDir: string): string[] => {
   return fs.readdirSync(baseDir)
 }
 
-const readScene = (filepath: string): any => {
+const readScene = (filepath: string): BaseEntry => {
   const data = fs.readFileSync(filepath)
   return JSON.parse(data.toString())
 }
 
 const connectDB = () => {
   const CONNECTION_STRING = env.get('CONNECTION_STRING', undefined)
-  db.postgres.connect(CONNECTION_STRING)
+  return db.postgres.connect(CONNECTION_STRING)
 }
 
-const indexScene = async (saveData: any, tag: string) => {
+const indexScene = async (saveData: BaseEntry, tag: string) => {
   const email = await crypto.decrypt(saveData.contest.email)
   log.info(`${saveData.project.id} ${email}`)
 
-  return Scene.create({
+  return Scene.create<SceneAttributes>({
     id: saveData.project.id,
     value: saveData,
     email: email,
@@ -54,20 +60,15 @@ const indexScene = async (saveData: any, tag: string) => {
     parcels_count: saveData.project.parcels.length,
     triangles_count: saveData.scene.metrics.triangles,
     items_count: Object.keys(saveData.scene.entities).length,
+    sessions: 0,
+    days: 0,
+    n_events: 0,
+    create_time: 0,
     tag: tag
   })
 }
 
-const getNextArg = () => {
-  return process.argv.pop() || ''
-}
-
-async function main() {
-  connectDB()
-
-  const tag = process.argv.length > 3 ? getNextArg() : ''
-  const baseDir = process.argv.length > 2 ? getNextArg() : ''
-
+const indexScenesFolder = async (baseDir: string, tag: string) => {
   log.info(`Reading scenes from:${baseDir} tag:${tag}`)
 
   const filenames = getSceneList(baseDir)
@@ -76,8 +77,36 @@ async function main() {
       const saveData = readScene(path.join(baseDir, filename))
       await indexScene(saveData, tag)
     } catch (err) {
-      log.error(err)
+      log.error(`Failed trying to index scene ${filename}. Error: ${err}`)
     }
+  }
+}
+
+const printHelp = () => {
+  console.log(`Index contest scenes into a db for easier querying.
+
+  usage: npm run indexScenes [scenes-folder] [tag]
+
+  positional arguments:
+    scene-folder  a folder in the filesystem that contains all the scene files
+    tag           a string tag to identify a batch of scenes
+  `)
+}
+
+const getArgList = () => {
+  const args = process.argv
+  return args.slice(2, args.length)
+}
+
+async function main() {
+  await connectDB()
+
+  const args = getArgList()
+  if (args.length >= 2) {
+    const [baseDir, tag] = args
+    await indexScenesFolder(baseDir, tag)
+  } else {
+    printHelp()
   }
 }
 
--- scripts/scenes.sql
+++ scripts/scenes.sql
@@ -2,19 +2,19 @@ DROP TABLE IF EXISTS scenes;
 
 CREATE TABLE scenes (
   id TEXT NOT NULL PRIMARY KEY, 
-  value JSONB,
+  value JSONB NOT NULL,
   email TEXT NOT NULL,
   title TEXT NOT NULL,
   description TEXT,
   address TEXT,
-  parcels_count INTEGER,
-  triangles_count INTEGER,
-  items_count INTEGER,
-  sessions INTEGER,
-  days INTEGER,
-  n_events INTEGER,
-  create_time INTEGER,
-  tag TEXT,
+  parcels_count INTEGER NOT NULL,
+  triangles_count INTEGER NOT NULL,
+  items_count INTEGER NOT NULL,
+  sessions INTEGER NOT NULL,
+  days INTEGER NOT NULL,
+  n_events INTEGER NOT NULL,
+  create_time INTEGER NOT NULL,
+  tag TEXT NOT NULL,
   created_at TIMESTAMP,
   updated_at TIMESTAMP
 );

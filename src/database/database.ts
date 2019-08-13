8dee59d55b3958f5307254d66cbc759548eee95e
Nicolás Santángelo <santangelonicolas@gmail.com>
2019-08-13 21:16:29 +0200

    feat: dates as utc

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,9 +1,15 @@
+import * as pgLib from 'pg'
 import { db } from 'decentraland-server'
 import { env } from 'decentraland-commons'
 
 const pg = db.clients.postgres
 
 export const database: typeof pg = Object.create(pg)
+
+pgLib.types.setTypeParser(1114, date => {
+  const utcStr = `${date}Z`
+  return new Date(utcStr).toISOString()
+})
 
 database.connect = async () => {
   const CONNECTION_STRING = env.get('CONNECTION_STRING', undefined)

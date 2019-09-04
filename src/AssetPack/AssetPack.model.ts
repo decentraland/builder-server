70a5a5d7ad62faa299f0eb946fb62828cae2b283
Nicolás Santángelo <santangelonicolas@gmail.com>
2019-09-04 12:55:10 -0300

    feat: upsert asset packs + assets

--- src/AssetPack/AssetPack.model.ts
+++ src/AssetPack/AssetPack.model.ts
@@ -5,6 +5,20 @@ import { AssetPackAttributes } from './AssetPack.types'
 
 export class AssetPack extends Model<AssetPackAttributes> {
   static tableName = 'asset_packs'
+  static async count(conditions: Partial<QueryPart>, extra?: string) {
+    return super.count({ is_deleted: false, ...conditions }, extra) // don't count deleted asset packs by default
+  }
+
+  static async delete(conditions: Partial<QueryPart>) {
+    if (!conditions.user_id) {
+      throw new Error('You need to supply an user_id to delete an asset pack')
+    }
+    return this.update({ is_deleted: true }, conditions)
+  }
+
+  static async hardDelete(conditions: Partial<AssetPackAttributes>) {
+    return this.db.delete(this.tableName, conditions)
+  }
 
   static async findVisible(userId: string | undefined) {
     const userIdQuery = userId ? SQL`user_id = ${userId}` : SQL`1 = 1`
@@ -34,17 +48,4 @@ export class AssetPack extends Model<AssetPackAttributes> {
 
     return counts[0].count > 0
   }
-
-  static async count(conditions: Partial<QueryPart>, extra?: string) {
-    return super.count({ is_deleted: false, ...conditions }, extra) // don't count deleted asset packs by default
-  }
-
-  static async delete(conditions: Partial<QueryPart>) {
-    if (!conditions.user_id) {
-      throw new Error('You need to supply an user_id to delete an asset pack')
-    }
-    return this.update({ is_deleted: true }, conditions)
-  }
-
-  // hardDelete
 }
--- src/AssetPack/AssetPack.router.ts
+++ src/AssetPack/AssetPack.router.ts
@@ -1,12 +1,22 @@
+import { utils } from 'decentraland-commons'
 import { server } from 'decentraland-server'
+import Ajv from 'ajv'
 
 import { Router } from '../common/Router'
 import { HTTPError, STATUS_CODES } from '../common/HTTPError'
-import { authentication, AuthRequest } from '../middleware'
-import { AssetPack } from './AssetPack.model'
+import { authentication, AuthRequest, modelExists } from '../middleware'
+import { modelAuthorization } from '../middleware/authorization'
+import { Ownable } from '../Ownable'
+import { Asset, AssetAttributes, assetPackSchema } from '../Asset'
+
+const BLACKLISTED_PROPERTIES = ['is_deleted']
+const ajv = new Ajv()
 
 export class AssetPackRouter extends Router {
   mount() {
+    const assetPackExists = modelExists(AssetPack)
+    const assetPackAuthorization = modelAuthorization(AssetPack)
+
     /**
      * Get all asset packs
      */
@@ -18,18 +28,30 @@ export class AssetPackRouter extends Router {
     this.router.get('/assetPacks/:id', server.handleRequest(this.getAssetPack))
 
     /**
+     * Upsert an asset pack and its assets
+     */
+    this.router.put(
+      '/assetPacks/:id',
+      authentication,
+      server.handleRequest(this.upsertAssetPack)
+    )
+
+    /**
      * Delete asset pack
      */
     this.router.delete(
       '/assetPacks/:id',
       authentication,
+      assetPackExists,
+      assetPackAuthorization,
       server.handleRequest(this.deleteAssetPack)
     )
   }
 
   async getAssetPacks(req: AuthRequest) {
     const user_id = req.auth ? req.auth.sub : ''
-    return AssetPack.findVisible(user_id)
+    const assetPacks = await AssetPack.findVisible(user_id)
+    return this.sanitize(assetPacks)
   }
 
   async getAssetPack(req: AuthRequest) {
@@ -46,9 +68,9 @@ export class AssetPackRouter extends Router {
       )
     }
 
-    const assetPack = await AssetPack.findWithAssets(id)
+    const assetPacks = await AssetPack.findWithAssets(id)
 
-    if (!assetPack) {
+    if (assetPacks.length === 0) {
       throw new HTTPError(
         'Asset pack not found',
         { id, user_id },
@@ -56,7 +78,43 @@ export class AssetPackRouter extends Router {
       )
     }
 
-    return assetPack
+    return this.sanitize(assetPacks)
+  }
+
+  async upsertAssetPack(req: AuthRequest) {
+    const id = server.extractFromReq(req, 'id')
+    const assetPackJSON: any = server.extractFromReq(req, 'assetPack')
+    const user_id = req.auth.sub
+
+    const validator = ajv.compile(assetPackSchema)
+    validator(assetPackJSON)
+
+    if (validator.errors) {
+      throw new HTTPError('Invalid schema', validator.errors)
+    }
+
+    const canUpsert = await new Ownable(AssetPack).canUpsert(id, user_id)
+    if (!canUpsert) {
+      throw new HTTPError('Unauthorized user', { id, user_id })
+    }
+
+    const assets: AssetAttributes[] = utils.pick(assetPackJSON, ['assets'])
+    const attributes = {
+      ...utils.omit(assetPackJSON, ['assets']),
+      user_id
+    } as AssetPackAttributes
+
+    if (id !== attributes.id) {
+      throw new HTTPError('The body and URL assetPack ids do not match', {
+        urlId: id,
+        bodyId: attributes.id
+      })
+    }
+
+    const upsertResult = await new AssetPack(attributes).upsert()
+    await Promise.all(assets.map(asset => new Asset(asset).upsert))
+
+    return upsertResult
   }
 
   async deleteAssetPack(req: AuthRequest) {
@@ -64,5 +122,12 @@ export class AssetPackRouter extends Router {
     const user_id = req.auth.sub
     await AssetPack.delete({ id, user_id })
     return true
+  }
+
+  private sanitize(assetPacks: AssetPackAttributes[]) {
+    return utils.mapOmit<AssetPackAttributes>(
+      assetPacks,
+      BLACKLISTED_PROPERTIES
+    )
   }
 }

db16f4d809848565c29daadd5d2ec33e62f4f45b
Nicolás Santángelo <santangelonicolas@gmail.com>
2019-09-09 17:34:25 -0300

    feat: update S3 manifests

--- scripts/updateProjectThumbnails.ts
+++ scripts/updateProjectThumbnails.ts
@@ -1,6 +1,9 @@
 import { db } from '../src/database'
 import { Project, THUMBNAIL_FILE_NAME } from '../src/Project'
 import { Pool } from '../src/Pool'
+import { S3Project, ACL, MANIFEST_FILENAME, POOL_FILENAME } from '../src/S3'
+
+const thumbnail = `${THUMBNAIL_FILE_NAME}.png`
 
 export async function updateProjectThumbnails() {
   console.log('==== Projects ====')
@@ -8,6 +11,9 @@ export async function updateProjectThumbnails() {
 
   console.log('==== Pools ====')
   await updateThumbnails(Pool)
+
+  console.log('==== S3 ====')
+  await updateS3()
 }
 
 async function updateThumbnails(Model: typeof Project | typeof Pool) {
@@ -15,13 +21,62 @@ async function updateThumbnails(Model: typeof Project | typeof Pool) {
 
   const updates = []
   for (const resource of resources) {
-    const thumbnail = `${THUMBNAIL_FILE_NAME}.png`
-
     console.log(`Updating project ${resource.id} with thumbnail "${thumbnail}"`)
     updates.push(Model.update({ thumbnail }, { id: resource.id }))
   }
 
   return Promise.all(updates)
+}
+
+async function updateS3() {
+  const projects = await Project.find()
+
+  const updates = []
+
+  for (const project of projects) {
+    const s3Project = new S3Project(project.id)
+
+    const [manifest, poolManifest] = await Promise.all([
+      s3Project.readFileBody(MANIFEST_FILENAME),
+      s3Project.readFileBody(POOL_FILENAME)
+    ])
+
+    if (manifest) {
+      updates.push(
+        updateManifestThumbnail(manifest.toString(), MANIFEST_FILENAME)
+      )
+    }
+
+    if (poolManifest) {
+      updates.push(
+        updateManifestThumbnail(poolManifest.toString(), POOL_FILENAME)
+      )
+    }
+  }
+
+  return Promise.all(updates)
+}
+
+async function updateManifestThumbnail(
+  manifest: string,
+  manifestFilename: string
+) {
+  const manifestJSON = JSON.parse(manifest)
+  const project = manifestJSON.project
+
+  if (project.thumbnail) {
+    project.thumbnail = thumbnail
+
+    console.log(
+      `Updating manifest "${manifestFilename}" for project ${project.id}`
+    )
+    return new S3Project(project.id).saveFile(
+      manifestFilename,
+      Buffer.from(JSON.stringify(manifestJSON)),
+      ACL.private
+    )
+  }
+  return Promise.resolve()
 }
 
 if (require.main === module) {

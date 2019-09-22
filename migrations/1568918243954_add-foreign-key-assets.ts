e7425059e9275dccdb04d51b21286d3ebb7e87ea
Nicolas Santangelo <santangelonicolas@gmail.com>
2019-09-22 12:13:46 -0300

    feat: add FK to assets and ON DELETE cascade (#65)

---
+++ migrations/1568918243954_add-foreign-key-assets.ts
@@ -0,0 +1,15 @@
+import { MigrationBuilder } from 'node-pg-migrate'
+import { Asset } from '../src/Asset'
+import { AssetPack } from '../src/AssetPack'
+
+const tableName = Asset.tableName
+
+export const up = (pgm: MigrationBuilder) => {
+  pgm.addConstraint(tableName, 'asset_pack_id_foreign_key', {
+    foreignKeys: {
+      columns: ['asset_pack_id'],
+      references: AssetPack.tableName,
+      onDelete: 'CASCADE'
+    }
+  })
+}

7720be6c4f8ede0f6a2ccced6ffcf7bd3ffb05c2
Nicolas Santangelo <santangelonicolas@gmail.com>
2019-09-27 16:51:18 -0300

    feat: add compound ids to Asset table (#70)

---
+++ migrations/1569598246522_add-compound-id.ts
@@ -0,0 +1,21 @@
+import { MigrationBuilder } from 'node-pg-migrate'
+import { Asset } from '../src/Asset'
+
+const tableName = Asset.tableName
+const columns = ['id', 'asset_pack_id']
+
+export const up = (pgm: MigrationBuilder) => {
+  pgm.dropConstraint(tableName, 'assets_pkey')
+
+  pgm.addConstraint(tableName, 'assets_pkey', {
+    primaryKey: columns
+  })
+
+  pgm.addIndex(tableName, columns, { unique: true })
+}
+
+export const down = (pgm: MigrationBuilder) => {
+  pgm.dropConstraint(tableName, 'assets_pkey')
+  pgm.addConstraint(tableName, 'assets_pkey', { primaryKey: 'id' })
+  pgm.dropIndex(tableName, columns)
+}

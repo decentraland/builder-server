78e939b0960c02d75562aa362a3ebf19ae2431f8
Fede <i@2fd.me>
2019-12-04 14:29:04 -0300

    feat: add statictis columns

---
+++ migrations/1575477950613_add-statistics-to-projects.ts
@@ -0,0 +1,21 @@
+import { MigrationBuilder } from 'node-pg-migrate'
+import { Project } from '../src/Project'
+
+const tableName = Project.tableName
+
+export const up = (pgm: MigrationBuilder) => {
+  const statisticsType = {
+    type: 'INTEGER',
+    notNull: false,
+    default: null
+  }
+
+  pgm.addColumns(tableName, {
+    transforms: statisticsType,
+    gltf_shapes: statisticsType,
+    nft_shapes: statisticsType,
+    scripts: statisticsType,
+    parcels: statisticsType,
+    entities: statisticsType
+  })
+}
---
+++ migrations/1575478542704_add-statistics-to-pools.ts
@@ -0,0 +1,21 @@
+import { MigrationBuilder } from 'node-pg-migrate'
+import { Pool } from '../src/Pool'
+
+const tableName = Pool.tableName
+
+export const up = (pgm: MigrationBuilder) => {
+  const statisticsType = {
+    type: 'INTEGER',
+    notNull: false,
+    default: null
+  }
+
+  pgm.addColumns(tableName, {
+    transforms: statisticsType,
+    gltf_shapes: statisticsType,
+    nft_shapes: statisticsType,
+    scripts: statisticsType,
+    parcels: statisticsType,
+    entities: statisticsType
+  })
+}

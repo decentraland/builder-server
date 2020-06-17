e28dbb3bb91d3295f8b826758bd066aff773893f
Ignacio Mazzara <nachomazzara@gmail.com>
2020-06-17 16:45:09 -0300

    feat: docker migrate

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-npm run migrate up || exit 1
-echo "migration run"
-./migrations/node-pg-migrate --database-url-var CONNECTION_STRING --migration-file-language ts --migrations-dir /app/migrations --ignore-pattern '\\..*|.*migrate(.ts)?' up
+npm run migrate:docker up || exit 1
 npm run seed || exit 1
 npm run migrate-assetpacks || exit 1
 npm run start || exit 1

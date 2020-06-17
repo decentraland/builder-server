044b4b63c80033ee018be4cde16bafb1b8b95d4d
Ignacio Mazzara <nachomazzara@gmail.com>
2020-06-17 15:47:53 -0300

    feat: new docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-npm run build || exit 1
 npm run migrate up || exit 1
 echo "migration run"
 ./migrations/node-pg-migrate --database-url-var CONNECTION_STRING --migration-file-language ts --migrations-dir /app/migrations --ignore-pattern '\\..*|.*migrate(.ts)?' up

cb6769c055d975ae886485276e8619b7bc24c9da
Ignacio Mazzara <nachomazzara@gmail.com>
2020-06-17 12:28:16 -0300

    feat: migrate by hand

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@
 npm run build || exit 1
 npm run migrate up || exit 1
 echo "migration run"
+./migrations/node-pg-migrate --database-url-var CONNECTION_STRING --migration-file-language ts --migrations-dir /app/migrations --ignore-pattern \\..*|.*migrate(.ts)? up
 npm run seed || exit 1
 npm run migrate-assetpacks || exit 1
 npm run start || exit 1

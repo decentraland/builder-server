e94e32775b3aeb91d9949414d8a9625618e456ed
Juan Cazala <juan@decentraland.org>
2020-09-04 20:36:42 -0300

    fix: change salt to text (#120)

---
+++ migrations/1599171601308_alter-collections-salt.ts
@@ -0,0 +1,11 @@
+import { MigrationBuilder } from 'node-pg-migrate'
+import { Collection } from '../src/Collection'
+
+const tableName = Collection.tableName
+
+export const up = (pgm: MigrationBuilder) => {
+  pgm.alterColumn(tableName, 'salt', {
+    type: 'TEXT',
+    notNull: false
+  })
+}
--- src/ethereum/types.ts
+++ src/ethereum/types.ts
@@ -5,10 +5,11 @@ export enum Network {
 
 export const FACTORY_COLLECTION_ADDRESS = {
   [Network.MAINNET]: '',
-  [Network.ROPSTEN]: ''
+  [Network.ROPSTEN]: '0x16d8bac5b67a6b782a9081377bec413bc5bb56a6'
 }
 
 export const FACTORY_COLLECTION_CODE_HASH = {
   [Network.MAINNET]: '',
-  [Network.ROPSTEN]: ''
+  [Network.ROPSTEN]:
+    '0x65cb40b83a99516a3dc696b3e8805681c6fa9324249b5e0ff9eb041314e3d2e8'
 }

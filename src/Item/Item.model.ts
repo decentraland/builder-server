a42c31481c1a189510b0425733375e0aeff2d88e
Nicolás Santángelo <santangelonicolas@gmail.com>
2020-10-01 19:50:22 -0300

    feat: return collectionless items

--- src/Item/Item.model.ts
+++ src/Item/Item.model.ts
@@ -10,7 +10,7 @@ export class Item extends Model<ItemAttributes> {
     return this.query<CollectionItemAttributes>(SQL`
       SELECT items.*, row_to_json(collections.*) as collection
         FROM ${SQL.raw(this.tableName)} as items
-        JOIN ${SQL.raw(
+        LEFT JOIN ${SQL.raw(
           Collection.tableName
         )} as collections ON collections.id = items.collection_id
         WHERE items.eth_address = ${ethAddress}`)

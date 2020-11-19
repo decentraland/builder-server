4cf2048a66cbfb96bac3140e58e641bf962a9a80
Juan Cazala <juan@decentraland.org>
2020-11-19 19:10:53 -0300

    chore: use this

--- src/Collection/Collection.model.ts
+++ src/Collection/Collection.model.ts
@@ -8,14 +8,14 @@ export class Collection extends Model<CollectionAttributes> {
   static findByEthAddress(ethAddress: string) {
     return this.query<CollectionAttributes>(SQL`
     SELECT *
-      FROM ${raw(Collection.tableName)}
+      FROM ${raw(this.tableName)}
       WHERE eth_address = ${ethAddress}`)
   }
 
   static findByIds(ids: string[]) {
     return this.query<CollectionAttributes>(SQL`
     SELECT *
-      FROM ${raw(Collection.tableName)}
+      FROM ${raw(this.tableName)}
       WHERE id = ANY(${ids})`)
   }
 }

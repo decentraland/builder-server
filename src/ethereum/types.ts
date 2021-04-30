630feb82fa37feba7f1596e90a3cd33a08a761bb
Nicolás Santángelo <santangelonicolas@gmail.com>
2021-04-30 02:22:14 -0300

    feat: new values

--- src/ethereum/types.ts
+++ src/ethereum/types.ts
@@ -3,18 +3,18 @@ export enum Network {
   ROPSTEN = 'ropsten',
 }
 
-export const FACTORY_COLLECTION_ADDRESS = {
-  [Network.MAINNET]: '',
-  [Network.ROPSTEN]: '0x0657fA4a3B14E568b0D9D49910D2875C5B6620F0',
-}
-
 export const FACTORY_COLLECTION_CODE_HASH = {
   [Network.MAINNET]: '',
   [Network.ROPSTEN]:
-    '0x9ac4948955f04134f33bda446b161eb8aeb1d21d6940a0c765795a09220c9b8f',
+    '0xb5b5b44c3ea9413e94e7b28af51d8cb84fd5c85643fbd9ec08a61e2a6f0886c8',
+}
+
+export const FACTORY_COLLECTION_ADDRESS = {
+  [Network.MAINNET]: '',
+  [Network.ROPSTEN]: '0x2C3212DEae0554E253e91cBa2B36A6ee888483C6',
 }
 
 export const FORWARDER_CONTRACT_ADDRESS = {
   [Network.MAINNET]: '',
-  [Network.ROPSTEN]: '0xe11663Feb948892F2d2D966bFCB40ba2F5C02Bb5',
+  [Network.ROPSTEN]: '0x0053e887b0F73e3aED2973968d5e85F33d305cbD',
 }

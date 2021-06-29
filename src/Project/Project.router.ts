24b758cf04cc0a1a10bb5ab3a2f66676f4454076
Suduck <adrian@suduck.com>
2021-06-29 13:06:31 +0200

    feat: add function to remove coords link

--- src/Project/Project.router.ts
+++ src/Project/Project.router.ts
@@ -79,6 +79,15 @@ export class ProjectRouter extends Router {
       server.handleRequest(this.deleteProject)
     )
 
+    /**
+     * Update all projects with coords to null
+     */
+    this.router.post(
+      '/projects/coords/:coords',
+      withAuthentication,
+      server.handleRequest(this.removeCoordsFromProjects)
+    )
+
     this.router.get(
       '/projects/:id/public',
       withProjectExistsAndIsPublic,
@@ -156,6 +165,13 @@ export class ProjectRouter extends Router {
   async deleteProject(req: AuthRequest) {
     const id = server.extractFromReq(req, 'id')
     await Project.update({ updated_at: new Date(), is_deleted: true }, { id })
+    return true
+  }
+
+  async removeCoordsFromProjects(req: AuthRequest) {
+    const eth_address = req.auth.ethAddress
+    const creation_coords = server.extractFromReq(req, 'coords')
+    await Project.update({ updated_at: new Date(), creation_coords: undefined }, { creation_coords, eth_address })
     return true
   }
 

3b2f3ba0f877258c903ddc9ec3e68e1ba5d5656d
Nicolás Santángelo <santangelonicolas@gmail.com>
2021-07-15 17:32:10 -0300

    fix: spare old default asset ids

--- migrations/1626375262726_dedup-asset-ids.ts
+++ migrations/1626375262726_dedup-asset-ids.ts
@@ -1,34 +1,35 @@
 import { MigrationBuilder } from 'node-pg-migrate'
 import { getAssetPackFiles } from '../scripts/seed/utils'
 import { Asset } from '../src/Asset'
+import { AssetPack, getDefaultEthAddress } from '../src/AssetPack'
 
-const tableName = Asset.tableName
+const assetTableName = Asset.tableName
+const assetPackTableName = AssetPack.tableName
 
 export const up = async (pgm: MigrationBuilder) => {
   const assetPackFiles = await getAssetPackFiles()
-  const assetPackIds: string[] = []
 
   for (const { data: assetPack } of assetPackFiles) {
-    assetPackIds.push(`'${assetPack.id}'`)
-
     for (const asset of assetPack.assets) {
-      pgm.sql(`UPDATE ${tableName}
+      pgm.sql(`UPDATE ${assetTableName}
         SET id = '${asset.id}'
         WHERE id = '${asset.legacy_id}' AND asset_pack_id = '${assetPack.id}'`)
     }
   }
 
-  pgm.sql(`UPDATE ${tableName}
-        SET id = uuid_generate_v4()
-        WHERE asset_pack_id NOT IN (${assetPackIds.join(',')})`)
+  pgm.sql(`UPDATE ${assetTableName}
+    SET a.id = uuid_generate_v4()
+    FROM ${assetTableName} a
+    JOIN ${assetPackTableName} b ON a.asset_pack_id = b.id
+    WHERE LENGTH(a.id) > 36  -- Fixed UUID length
+      OR b.eth_address != '${getDefaultEthAddress()}'`)
 }
 
 export const down = async (pgm: MigrationBuilder) => {
   const assetPackFiles = await getAssetPackFiles()
-
   for (const { data: assetPack } of assetPackFiles) {
     for (const asset of assetPack.assets) {
-      pgm.sql(`UPDATE ${tableName}
+      pgm.sql(`UPDATE ${assetTableName}
         SET id = '${asset.legacy_id}'
         WHERE id = '${asset.id}'`)
     }

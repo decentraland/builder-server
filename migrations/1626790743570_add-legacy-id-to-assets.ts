2c311218a21d74cfc1252d590b9461d34511fc73
Nicolás Santángelo <santangelonicolas@gmail.com>
2021-07-20 11:58:51 -0300

    feat: restore uuids and add legacy ids to db

---
+++ migrations/1626790743570_add-legacy-id-to-assets.ts
@@ -0,0 +1,16 @@
+import { MigrationBuilder } from 'node-pg-migrate'
+import { Asset } from '../src/Asset'
+
+const tableName = Asset.tableName
+
+export const up = (pgm: MigrationBuilder) => {
+  pgm.addColumn(tableName, {
+    legacy_id: { type: 'TEXT' },
+  })
+
+  pgm.sql(`UPDATE ${tableName} SET legacy_id = ${tableName}.id`)
+}
+
+export const down = (pgm: MigrationBuilder) => {
+  pgm.dropColumn(tableName, 'legacy_id')
+}

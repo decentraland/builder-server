a8cd1bbb6d2db91883eb1af86e2e73ac87d9b7f8
Lautaro Petaccio <lausuper@gmail.com>
2021-08-12 09:33:37 -0300

    Revert

--- src/common/Router.ts
+++ src/common/Router.ts
@@ -1,12 +1,18 @@
 import express = require('express')
+import { IMetricsComponent } from '@well-known-components/interfaces'
 
 import { ExpressApp } from './ExpressApp'
 
-export class Router {
-  protected router: express.Router
+export class Router<M extends string = string> {
+  protected readonly router: express.Router
+  protected readonly metrics: IMetricsComponent<M> | undefined
 
-  constructor(router: ExpressApp | express.Router) {
+  constructor(
+    router: ExpressApp | express.Router,
+    metrics?: IMetricsComponent<M>
+  ) {
     this.router = router instanceof ExpressApp ? router.getRouter() : router
+    this.metrics = metrics
   }
 
   mount(): void {

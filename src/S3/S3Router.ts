ed8815bcb3fad1ab1d9707bb58e5c7a28cc1939d
Lautaro Petaccio <1120791+LautaroPetaccio@users.noreply.github.com>
2021-08-31 16:47:39 -0300

    fix: Improve cache for contents (#264)
    * fix: Improve cache for contents
    
    * Split handlers

--- src/S3/S3Router.ts
+++ src/S3/S3Router.ts
@@ -5,31 +5,35 @@ import { Router } from '../common/Router'
 import { getBucketURL } from './s3'
 import { S3AssetPack } from './S3AssetPack'
 import { S3Content } from './S3Content'
+import { S3Model } from './S3Model'
 
 export class S3Router extends Router {
   mount() {
     /**
      * Get an asset pack file by file id
      */
-    this.router.get(
-      '/storage/assetPacks/:filename',
-      this.getHandlerForAssetType(S3AssetPack)
-    )
+    this.router.get('/storage/assetPacks/:filename', this.handleAssetPacks)
 
     /**
      * Get an asset file by file id (also contains items)
      */
-    this.router.get(
-      '/storage/contents/:filename',
-      this.getHandlerForAssetType(S3Content)
-    )
+    this.router.get('/storage/contents/:filename', this.handleContents)
   }
 
-  private getHandlerForAssetType(Model: typeof S3AssetPack | typeof S3Content) {
-    const model = new Model('')
-    return async (req: Request, res: Response) => {
-      const filename = server.extractFromReq(req, 'filename')
-      return res.redirect(`${getBucketURL()}/${model.getFileKey(filename)}`)
-    }
+  private buildRedirectUrl(model: S3Model, filename: string) {
+    return `${getBucketURL()}/${model.getFileKey(filename)}`
+  }
+
+  private handleAssetPacks(req: Request, res: Response) {
+    const model = new S3AssetPack('')
+    const filename = server.extractFromReq(req, 'filename')
+    return res.redirect(this.buildRedirectUrl(model, filename))
+  }
+
+  private handleContents(req: Request, res: Response) {
+    const model = new S3Content()
+    const filename = server.extractFromReq(req, 'filename')
+    res.setHeader('Cache-Control', 'public,max-age=31536000,immutable')
+    return res.redirect(this.buildRedirectUrl(model, filename), 301)
   }
 }

f9f591a02c7baa7c0029edb1a3059670b1ed4206
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2021-09-17 17:10:57 -0300

    refactor: Refactor tests

--- src/Item/Item.router.spec.ts
+++ src/Item/Item.router.spec.ts
@@ -10,74 +10,110 @@ import { ItemAttributes } from '.'
 
 jest.mock('../common/Router')
 jest.mock('../common/ExpressApp')
-// jest.mock('../Ownable')
-
-// console.log(Item)
-
-// const mockOwnable = Ownable as jest.Mock
-
-const validItem = {
-  id: 'a8aca0ee-b3f6-4a8e-a78c-d8efeb099cd9',
-  name: 'name',
-  description: 'description',
-  eth_address: 'eth_address',
-  type: 'wearable',
-  contents: {},
-  created_at: 'created_at',
-  updated_at: 'updated_at',
-  metrics: {
-    meshes: 1,
-    bodies: 1,
-    materials: 1,
-    textures: 1,
-    triangles: 1,
-    entities: 1,
-  },
-  data: {
-    replaces: ['eyebrows'],
-    hides: ['eyebrows'],
-    tags: ['tags'],
-    representations: [
-      {
-        mainFile: 'mainFile',
-        contents: ['contents'],
-        overrideReplaces: ['eyebrows'],
-        overrideHides: ['eyebrows'],
-        bodyShapes: ['urn:decentraland:off-chain:base-avatars:BaseMale'],
-      },
-    ],
-  },
-}
 
 describe('when upsertItem is called', () => {
-  let itemFindOneSpy = jest.spyOn(Item, 'findOne')
-  let collectionFindOneSpy = jest.spyOn(Collection, 'findOne')
-  let ownableCanUpsertSpy = jest.spyOn(Ownable.prototype, 'canUpsert')
+  const testCollectionId = 'ffb11be4-94f0-47a6-bf1c-6d77fbbea1d3'
 
-  beforeEach(() => {
-    // mockOwnable.mockImplementation(() => ({
-    //   canUpsert: () => Promise.resolve(true),
-    // }))
+  const testItem = {
+    id: 'a8aca0ee-b3f6-4a8e-a78c-d8efeb099cd9',
+    name: 'name',
+    description: 'description',
+    eth_address: 'eth_address',
+    type: 'wearable',
+    contents: {},
+    created_at: 'created_at',
+    updated_at: 'updated_at',
+    metrics: {
+      meshes: 1,
+      bodies: 1,
+      materials: 1,
+      textures: 1,
+      triangles: 1,
+      entities: 1,
+    },
+    data: {
+      replaces: ['eyebrows'],
+      hides: ['eyebrows'],
+      tags: ['tags'],
+      representations: [
+        {
+          mainFile: 'mainFile',
+          contents: ['contents'],
+          overrideReplaces: ['eyebrows'],
+          overrideHides: ['eyebrows'],
+          bodyShapes: ['urn:decentraland:off-chain:base-avatars:BaseMale'],
+        },
+      ],
+    },
+  }
+
+  const itemFindOneSpy = jest.spyOn(Item, 'findOne')
+  const collectionFindOneSpy = jest.spyOn(Collection, 'findOne')
+  const ownableCanUpsertSpy = jest.spyOn(Ownable.prototype, 'canUpsert')
+
+  beforeAll(() => {
     ownableCanUpsertSpy.mockResolvedValue(true)
     itemFindOneSpy.mockResolvedValue(undefined)
     collectionFindOneSpy.mockResolvedValue(undefined)
   })
 
-  afterEach(() => {
-    jest.resetAllMocks()
-  })
+  const testError = (req: any, message: string) => {
+    const app = new ExpressApp()
+    const router = new ItemRouter(app)
+
+    return expect(router.upsertItem(req)).rejects.toThrowError(message)
+  }
+
+  const testSuccess = (req: any) => {
+    jest
+      .spyOn(Item.prototype, 'upsert')
+      .mockResolvedValueOnce({} as ItemAttributes)
+
+    const app = new ExpressApp()
+    const router = new ItemRouter(app)
+
+    return expect(router.upsertItem(req)).resolves.toStrictEqual({})
+  }
+
+  const mockIsCollectionPublished = () => {
+    collectionFindOneSpy.mockResolvedValueOnce({
+      collection_id: testCollectionId,
+      eth_address: testItem.eth_address,
+    })
+    
+    jest
+      .spyOn(collectionAPI, 'fetchCollectionWithItemsByContractAddress')
+      .mockResolvedValueOnce({
+        collection: {} as CollectionFragment,
+        items: [{}] as ItemFragment[],
+      })
+
+    jest
+      .spyOn(peerAPI, 'fetchWearables')
+      .mockResolvedValueOnce([{}] as Wearable[])
+  }
+
+  const mockItemFindOne = (merge: any = {}) => {
+    itemFindOneSpy.mockResolvedValueOnce({
+      ...testItem,
+      collection_id: testCollectionId,
+      ...merge,
+    })
+  }
 
   describe('when param id is different from payload id', () => {
     it('should fail with body and url ids do not match message', async () => {
-      const app = new ExpressApp()
-      const router = new ItemRouter(app)
-
-      const req: any = {
-        query: { id: 'id' },
-        body: { item: { id: 'different id' } },
-      }
-
-      await expect(router.upsertItem(req)).rejects.toThrowError(
+      await testError(
+        {
+          query: {
+            id: 'id',
+          },
+          body: {
+            item: {
+              id: 'different id',
+            },
+          },
+        },
         'The body and URL item ids do not match'
       )
     })
@@ -85,16 +121,20 @@ describe('when upsertItem is called', () => {
 
   describe('when payload schema is invalid', () => {
     it('should fail with invalid schema', async () => {
-      const app = new ExpressApp()
-      const router = new ItemRouter(app)
-
-      const req: any = {
-        query: { id: 'id' },
-        body: { item: { id: 'id' } },
-        auth: { ethAddress: 'ethAddress' },
-      }
-
-      await expect(router.upsertItem(req)).rejects.toThrowError(
+      await testError(
+        {
+          query: {
+            id: 'id',
+          },
+          body: {
+            item: {
+              id: 'id',
+            },
+          },
+          auth: {
+            ethAddress: 'ethAddress',
+          },
+        },
         'Invalid schema'
       )
     })
@@ -102,16 +142,21 @@ describe('when upsertItem is called', () => {
 
   describe('when is_approved is sent in the payload', () => {
     it('should fail with cant set is_approved message', async () => {
-      const app = new ExpressApp()
-      const router = new ItemRouter(app)
-
-      const req: any = {
-        query: { id: validItem.id },
-        body: { item: { ...validItem, is_approved: true } },
-        auth: { ethAddress: 'ethAddress' },
-      }
-
-      await expect(router.upsertItem(req)).rejects.toThrowError(
+      await testError(
+        {
+          query: {
+            id: testItem.id,
+          },
+          body: {
+            item: {
+              ...testItem,
+              is_approved: true,
+            },
+          },
+          auth: {
+            ethAddress: 'ethAddress',
+          },
+        },
         'Can not change is_published or is_approved property'
       )
     })
@@ -119,16 +164,21 @@ describe('when upsertItem is called', () => {
 
   describe('when is_published is sent in the payload', () => {
     it('should fail with cant set is_approved message', async () => {
-      const app = new ExpressApp()
-      const router = new ItemRouter(app)
-
-      const req: any = {
-        query: { id: validItem.id },
-        body: { item: { ...validItem, is_published: true } },
-        auth: { ethAddress: 'ethAddress' },
-      }
-
-      await expect(router.upsertItem(req)).rejects.toThrowError(
+      await testError(
+        {
+          query: {
+            id: testItem.id,
+          },
+          body: {
+            item: {
+              ...testItem,
+              is_published: true,
+            },
+          },
+          auth: {
+            ethAddress: 'ethAddress',
+          },
+        },
         'Can not change is_published or is_approved property'
       )
     })
@@ -138,16 +188,18 @@ describe('when upsertItem is called', () => {
     it('should fail with unauthorized user message', async () => {
       ownableCanUpsertSpy.mockResolvedValueOnce(false)
 
-      const app = new ExpressApp()
-      const router = new ItemRouter(app)
-
-      const req: any = {
-        query: { id: validItem.id },
-        body: { item: validItem },
-        auth: { ethAddress: 'ethAddress' },
-      }
-
-      await expect(router.upsertItem(req)).rejects.toThrowError(
+      await testError(
+        {
+          query: {
+            id: testItem.id,
+          },
+          body: {
+            item: testItem,
+          },
+          auth: {
+            ethAddress: 'ethAddress',
+          },
+        },
         'Unauthorized user'
       )
     })
@@ -155,69 +207,52 @@ describe('when upsertItem is called', () => {
 
   describe('when the item collection is being changed', () => {
     it('should fail with cant change item collection message', async () => {
-      itemFindOneSpy.mockImplementationOnce(() =>
-        Promise.resolve({
-          ...validItem,
-          collection_id: 'ffb11be4-94f0-47a6-bf1c-6d77fbbea1d3',
-        })
-      )
+      const differentCollectionId = '6d3fd719-57c1-4436-bec3-7dd954c3fbfe'
 
-      const app = new ExpressApp()
-      const router = new ItemRouter(app)
+      mockItemFindOne()
 
-      const req: any = {
-        query: { id: validItem.id },
-        body: {
-          item: {
-            ...validItem,
-            collection_id: '6d3fd719-57c1-4436-bec3-7dd954c3fbfe',
+      await testError(
+        {
+          query: {
+            id: testItem.id,
+          },
+          body: {
+            item: {
+              ...testItem,
+              collection_id: differentCollectionId,
+            },
+          },
+          auth: {
+            ethAddress: testItem.eth_address,
           },
         },
-        auth: { ethAddress: validItem.eth_address },
-      }
-
-      await expect(router.upsertItem(req)).rejects.toThrowError(
         "Item can't change between collections"
       )
     })
   })
 
   describe('when the collection is published', () => {
+    beforeEach(() => {
+      mockIsCollectionPublished()
+    })
+
     describe('when the item is being upserted for the first time', () => {
       it('should fail with can not add item to published collection message', async () => {
-        collectionFindOneSpy.mockResolvedValueOnce({
-          collection_id: '6d3fd719-57c1-4436-bec3-7dd954c3fbfe',
-          eth_address: validItem.eth_address,
-        })
-
-        jest
-          .spyOn(collectionAPI, 'fetchCollectionWithItemsByContractAddress')
-          .mockImplementation(() =>
-            Promise.resolve({
-              collection: {} as CollectionFragment,
-              items: [{}] as ItemFragment[],
-            })
-          )
-
-        jest
-          .spyOn(peerAPI, 'fetchWearables')
-          .mockImplementation(() => Promise.resolve([{}] as Wearable[]))
-
-        const app = new ExpressApp()
-        const router = new ItemRouter(app)
-
-        const req: any = {
-          query: { id: validItem.id },
-          body: {
-            item: {
-              ...validItem,
-              collection_id: '6d3fd719-57c1-4436-bec3-7dd954c3fbfe',
+        await testError(
+          {
+            query: {
+              id: testItem.id,
+            },
+            body: {
+              item: {
+                ...testItem,
+                collection_id: testCollectionId,
+              },
+            },
+            auth: {
+              ethAddress: testItem.eth_address,
             },
           },
-          auth: { ethAddress: validItem.eth_address },
-        }
-
-        await expect(router.upsertItem(req)).rejects.toThrowError(
           "Items can't be added to a published collection"
         )
       })
@@ -225,45 +260,20 @@ describe('when upsertItem is called', () => {
 
     describe('when the item is being removed from the collection', () => {
       it('should fail with can not remove item from published collection message', async () => {
-        itemFindOneSpy.mockImplementationOnce(() =>
-          Promise.resolve({
-            ...validItem,
-            collection_id: '6d3fd719-57c1-4436-bec3-7dd954c3fbfe',
-          })
-        )
+        mockItemFindOne()
 
-        collectionFindOneSpy.mockImplementationOnce(() =>
-          Promise.resolve({
-            collection_id: '6d3fd719-57c1-4436-bec3-7dd954c3fbfe',
-            eth_address: validItem.eth_address,
-          })
-        )
-
-        jest
-          .spyOn(collectionAPI, 'fetchCollectionWithItemsByContractAddress')
-          .mockImplementation(() =>
-            Promise.resolve({
-              collection: {} as CollectionFragment,
-              items: [{}] as ItemFragment[],
-            })
-          )
-
-        jest
-          .spyOn(peerAPI, 'fetchWearables')
-          .mockImplementation(() => Promise.resolve([{}] as Wearable[]))
-
-        const app = new ExpressApp()
-        const router = new ItemRouter(app)
-
-        const req: any = {
-          query: { id: validItem.id },
-          body: {
-            item: validItem,
+        await testError(
+          {
+            query: {
+              id: testItem.id,
+            },
+            body: {
+              item: testItem,
+            },
+            auth: {
+              ethAddress: testItem.eth_address,
+            },
           },
-          auth: { ethAddress: validItem.eth_address },
-        }
-
-        await expect(router.upsertItem(req)).rejects.toThrowError(
           "Items can't be removed from a pubished collection"
         )
       })
@@ -271,50 +281,27 @@ describe('when upsertItem is called', () => {
 
     describe("when the item's rarity is being changed", () => {
       it('should fail with can not update items rarity message', async () => {
-        itemFindOneSpy.mockImplementationOnce(() =>
-          Promise.resolve({
-            ...validItem,
-            rarity: 'mythic',
-            collection_id: '6d3fd719-57c1-4436-bec3-7dd954c3fbfe',
-          })
-        )
+        const rarity1 = 'mythic'
+        const rarity2 = 'unique'
 
-        collectionFindOneSpy.mockImplementationOnce(() =>
-          Promise.resolve({
-            collection_id: '6d3fd719-57c1-4436-bec3-7dd954c3fbfe',
-            eth_address: validItem.eth_address,
-          })
-        )
+        mockItemFindOne({ rarity: rarity1 })
 
-        jest
-          .spyOn(collectionAPI, 'fetchCollectionWithItemsByContractAddress')
-          .mockImplementation(() =>
-            Promise.resolve({
-              collection: {} as CollectionFragment,
-              items: [{}] as ItemFragment[],
-            })
-          )
-
-        jest
-          .spyOn(peerAPI, 'fetchWearables')
-          .mockImplementation(() => Promise.resolve([{}] as Wearable[]))
-
-        const app = new ExpressApp()
-        const router = new ItemRouter(app)
-
-        const req: any = {
-          query: { id: validItem.id },
-          body: {
-            item: {
-              ...validItem,
-              rarity: 'unique',
-              collection_id: '6d3fd719-57c1-4436-bec3-7dd954c3fbfe',
+        await testError(
+          {
+            query: {
+              id: testItem.id,
+            },
+            body: {
+              item: {
+                ...testItem,
+                rarity: rarity2,
+                collection_id: testCollectionId,
+              },
+            },
+            auth: {
+              ethAddress: testItem.eth_address,
             },
           },
-          auth: { ethAddress: validItem.eth_address },
-        }
-
-        await expect(router.upsertItem(req)).rejects.toThrowError(
           "An item rarity from a published collection can't be changed"
         )
       })
@@ -323,52 +310,23 @@ describe('when upsertItem is called', () => {
 
   describe('when everything is correct', () => {
     it('should resolve correctly', async () => {
-      itemFindOneSpy.mockImplementationOnce(() =>
-        Promise.resolve({
-          ...validItem,
-          collection_id: '6d3fd719-57c1-4436-bec3-7dd954c3fbfe',
-        })
-      )
+      mockItemFindOne()
+      mockIsCollectionPublished()
 
-      collectionFindOneSpy.mockImplementationOnce(() =>
-        Promise.resolve({
-          collection_id: '6d3fd719-57c1-4436-bec3-7dd954c3fbfe',
-          eth_address: validItem.eth_address,
-        })
-      )
-
-      jest
-        .spyOn(collectionAPI, 'fetchCollectionWithItemsByContractAddress')
-        .mockImplementation(() =>
-          Promise.resolve({
-            collection: {} as CollectionFragment,
-            items: [{}] as ItemFragment[],
-          })
-        )
-
-      jest
-        .spyOn(peerAPI, 'fetchWearables')
-        .mockImplementation(() => Promise.resolve([{}] as Wearable[]))
-
-      jest
-        .spyOn(Item.prototype, 'upsert')
-        .mockImplementation(() => Promise.resolve({} as ItemAttributes))
-
-      const app = new ExpressApp()
-      const router = new ItemRouter(app)
-
-      const req: any = {
-        query: { id: validItem.id },
+      await testSuccess({
+        query: {
+          id: testItem.id,
+        },
         body: {
           item: {
-            ...validItem,
-            collection_id: '6d3fd719-57c1-4436-bec3-7dd954c3fbfe',
+            ...testItem,
+            collection_id: testCollectionId,
           },
         },
-        auth: { ethAddress: validItem.eth_address },
-      }
-
-      await expect(router.upsertItem(req)).resolves.toStrictEqual({})
+        auth: {
+          ethAddress: testItem.eth_address,
+        },
+      })
     })
   })
 })

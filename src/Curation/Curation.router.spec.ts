5661385943bb957c82ee1461f40b7f7392f4209c
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2021-09-22 13:15:44 -0300

    chore: Add tests for curation router

---
+++ src/Curation/Curation.router.spec.ts
@@ -0,0 +1,177 @@
+import { Curation, CurationRouter } from '.'
+import { ExpressApp } from '../common/ExpressApp'
+import { isCommitteeMember } from '../Committee'
+import { hasAccess } from './access'
+import { getMergedCollection } from '../Collection/util'
+
+jest.mock('../common/Router')
+jest.mock('../common/ExpressApp')
+jest.mock('../Committee')
+jest.mock('../Collection/util')
+jest.mock('./access')
+
+const mockIsComiteeMember = isCommitteeMember as jest.Mock
+const mockHasAccess = hasAccess as jest.Mock
+const mockGetMergedCollection = getMergedCollection as jest.Mock
+
+describe('when handling a request', () => {
+  let router: CurationRouter
+
+  beforeEach(() => {
+    router = new CurationRouter(new ExpressApp())
+  })
+
+  afterEach(() => {
+    jest.restoreAllMocks()
+  })
+
+  describe('when trying to obtain a list of curations', () => {
+    describe('when the caller belongs to the commitee', () => {
+      it('should resolve with the collections provided by Curation.getAll', async () => {
+        mockIsComiteeMember.mockResolvedValueOnce(true)
+
+        const getAllSpy = jest
+          .spyOn(Curation, 'getAll')
+          .mockResolvedValueOnce([])
+
+        const req = {
+          auth: { ethAddress: 'ethAddress' },
+        } as any
+
+        await router.getCurations(req)
+
+        expect(getAllSpy).toHaveBeenCalled()
+      })
+    })
+
+    describe('when the caller does not belong to the commitee', () => {
+      it('should resolve with the collections provided by Curation.getAllForAddress', async () => {
+        mockIsComiteeMember.mockResolvedValueOnce(false)
+
+        const getAllForAddressSpy = jest
+          .spyOn(Curation, 'getAllForAddress')
+          .mockResolvedValueOnce([])
+
+        const req = {
+          auth: { ethAddress: 'ethAddress' },
+        } as any
+
+        await router.getCurations(req)
+
+        expect(getAllForAddressSpy).toHaveBeenCalled()
+      })
+    })
+  })
+
+  describe('when trying to obtain the latest curation for a collection', () => {
+    describe('when the caller has no access', () => {
+      it('should reject with an unauthorized message', async () => {
+        mockHasAccess.mockResolvedValueOnce(false)
+
+        const req = {
+          auth: { ethAddress: 'ethAddress' },
+          params: { collectionId: 'collectionId' },
+        } as any
+
+        await expect(router.getCuration(req)).rejects.toThrowError(
+          'Unauthorized'
+        )
+      })
+    })
+
+    describe('when everything is correct', () => {
+      it('should resolve with the expected curation', async () => {
+        mockHasAccess.mockResolvedValueOnce(true)
+        jest
+          .spyOn(Curation, 'getLatestForCollection')
+          .mockResolvedValueOnce({} as any)
+
+        const req = {
+          auth: { ethAddress: 'ethAddress' },
+          params: { collectionId: 'collectionId' },
+        } as any
+
+        await expect(router.getCuration(req)).resolves.toStrictEqual({})
+      })
+    })
+  })
+
+  describe('when trying to insert a new curation', () => {
+    describe('when the caller has no access', () => {
+      it('should reject with an unauthorized message', async () => {
+        mockHasAccess.mockResolvedValueOnce(false)
+
+        const req = {
+          auth: { ethAddress: 'ethAddress' },
+          params: { collectionId: 'collectionId' },
+        } as any
+
+        await expect(router.insertCuration(req)).rejects.toThrowError(
+          'Unauthorized'
+        )
+      })
+    })
+
+    describe('when the collection does not exist', () => {
+      it('should reject with collection not found message', async () => {
+        mockHasAccess.mockResolvedValueOnce(true)
+        mockGetMergedCollection.mockResolvedValueOnce({ collection: undefined })
+
+        const req = {
+          auth: { ethAddress: 'ethAddress' },
+          params: { collectionId: 'collectionId' },
+        } as any
+
+        await expect(router.insertCuration(req)).rejects.toThrowError(
+          'Collection does not exist'
+        )
+      })
+    })
+
+    describe('when the collection has a pending review', () => {
+      it('should reject with an ongoing review message', async () => {
+        mockHasAccess.mockResolvedValueOnce(true)
+
+        mockGetMergedCollection.mockResolvedValueOnce({
+          collection: { reviewed_at: new Date(2000, 0) },
+        })
+
+        jest
+          .spyOn(Curation, 'getLatestForCollection')
+          .mockResolvedValueOnce({ timestamp: new Date(2000, 1) } as any)
+
+        const req = {
+          auth: { ethAddress: 'ethAddress' },
+          params: { collectionId: 'collectionId' },
+        } as any
+
+        await expect(router.insertCuration(req)).rejects.toThrowError(
+          'There is already an ongoing review request for this collection'
+        )
+      })
+    })
+
+    describe('when everything is fine', () => {
+      it('should resolve with an upserted curation', async () => {
+        mockHasAccess.mockResolvedValueOnce(true)
+
+        mockGetMergedCollection.mockResolvedValueOnce({
+          collection: { reviewed_at: new Date(2000, 1) },
+        })
+
+        jest
+          .spyOn(Curation, 'getLatestForCollection')
+          .mockResolvedValueOnce({ timestamp: new Date(2000, 0) } as any)
+
+        jest.spyOn(Curation, 'upsert').mockResolvedValueOnce({} as any)
+
+        const req = {
+          auth: { ethAddress: 'ethAddress' },
+          params: { collectionId: 'collectionId' },
+        } as any
+
+        await expect(router.insertCuration(req)).resolves.toStrictEqual({})
+      })
+    })
+  })
+})

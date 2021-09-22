55584ae47b8ae20cf5a36073ae87f51f8b586bb8
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2021-09-21 21:51:09 -0300

    fix: Curation static functions

--- src/Curation/Curation.model.ts
+++ src/Curation/Curation.model.ts
@@ -4,16 +4,17 @@ import { CurationAttributes } from './Curation.types'
 export class Curation extends Model<CurationAttributes> {
   static tableName = 'curations'
 
-  static getAll = () =>
-    this.query<CurationAttributes>(SQL`
+  static getAll() {
+    return this.query<CurationAttributes>(SQL`
     SELECT DISTINCT ON (collection_id) * FROM ${raw(this.tableName)} AS c1
     WHERE timestamp = (
 	    SELECT MAX(timestamp) FROM ${raw(this.tableName)} AS c2
 	    WHERE c1.collection_id = c2.collection_id
     )`)
+  }
 
-  static getAllForAddress = (address: string) =>
-    this.query<CurationAttributes>(SQL`
+  static getAllForAddress(address: string) {
+    return this.query<CurationAttributes>(SQL`
     SELECT DISTINCT ON (collection_id) * FROM ${raw(this.tableName)} AS cu1
     WHERE collection_id IN (
       SLEECT collection_id FROM ${raw(this.tableName)} AS co
@@ -23,8 +24,9 @@ export class Curation extends Model<CurationAttributes> {
       SELECT max(timestamp) FROM ${raw(this.tableName)} AS cu2
       WHERE cu1.collection_id = cu2.collection_id
     )`)
+  }
 
-  static getLatestForCollection = async (collectionId: string) => {
+  static async getLatestForCollection(collectionId: string) {
     const query = SQL`
     SELECT DISTINCT ON (collection_id) * FROM ${raw(this.tableName)}
     WHERE collection_id = ${collectionId}

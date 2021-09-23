e696d5e45e9e66771f7b26d9b1d128f7e1c407e8
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2021-09-23 15:38:28 -0300

    Update DELETE_ME.spec.ts

--- src/DELETE_ME.spec.ts
+++ src/DELETE_ME.spec.ts
@@ -1,6 +1,7 @@
 /**
  * This test exists so that at least 1 test exists inside the repo
  * causing CI not to break
+ * Delete this file as soon as more tests are added
  */
 
 it('DELETE ME', () => {

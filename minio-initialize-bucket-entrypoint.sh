eb43b801df89cdee2024b3a0ef3310253a2aff4b
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2021-10-06 10:03:27 -0300

    chore: Fix echo

--- minio-initialize-bucket-entrypoint.sh
+++ minio-initialize-bucket-entrypoint.sh
@@ -5,7 +5,7 @@ sleep 5
 mc alias set minio http://minio:9000 admin password
 
 if mc find minio/builder-server ; then
-  echo "Bucket \"builder-server already exists, no need to create it again\""
+  echo "Bucket \"builder-server\" already exists, no need to create it again"
 else
   mc mb minio/builder-server
   mc policy set public minio/builder-server

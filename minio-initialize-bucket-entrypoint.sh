cb3843590bf08198b4b5e796eaa3b97c90bb6fba
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2021-10-06 11:43:30 -0300

    chore: Add comment on the bucket initialization script

--- minio-initialize-bucket-entrypoint.sh
+++ minio-initialize-bucket-entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Required as the minio service on the docker-compose file might
+# not be completely up on the execution of this file.
 sleep 5
 
 mc alias set minio http://minio:9000 admin password

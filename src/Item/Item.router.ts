7d6707587257dc4f347d2b0b8778ac511552de89
Lautaro Petaccio <1120791+LautaroPetaccio@users.noreply.github.com>
2021-10-06 13:11:06 -0300

    fix: Upsert item without converting it to a DB item (#296)
    * fix: Upsert item without converting it to a DB item
    
    * fix: PR fixes

--- src/Item/Item.router.ts
+++ src/Item/Item.router.ts
@@ -26,8 +26,10 @@ import {
 } from '../Collection'
 import { hasAccess as hasCollectionAccess } from '../Collection/access'
 import { isCommitteeMember } from '../Committee'
-import { FullItem, itemSchema } from './Item.types'
+import { FullItem } from './Item.types'
 import { hasAccess } from './access'
+import { toDBItem } from './utils'
+import { itemSchema } from './Item.schema'
 
 const validator = getValidator()
 
@@ -285,7 +287,7 @@ export class ItemRouter extends Router {
 
   async upsertItem(req: AuthRequest) {
     const id = server.extractFromReq(req, 'id')
-    const itemJSON: any = server.extractFromReq(req, 'item')
+    const itemJSON: FullItem = server.extractFromReq(req, 'item')
     const eth_address = req.auth.ethAddress.toLowerCase()
 
     const validate = validator.compile(itemSchema)
@@ -376,10 +378,10 @@ export class ItemRouter extends Router {
       }
     }
 
-    const attributes = {
+    const attributes = toDBItem({
       ...itemJSON,
       eth_address,
-    } as ItemAttributes
+    })
 
     if (id !== attributes.id) {
       throw new HTTPError('The body and URL item ids do not match', {
---
+++ src/Item/utils.ts
@@ -0,0 +1,11 @@
+import { utils } from 'decentraland-commons'
+import { FullItem, ItemAttributes } from './Item.types'
+
+export function toDBItem(item: FullItem): ItemAttributes {
+  return utils.omit(item, [
+    'is_published',
+    'is_approved',
+    'in_catalyst',
+    'total_supply',
+  ])
+}

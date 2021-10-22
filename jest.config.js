aaf2d8fa9aa62e7c084e624807fc525f230b0f37
Nicolás Santángelo <santangelonicolas@gmail.com>
2021-10-22 13:06:43 -0300

    chore: faster jest tests

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,16 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
+  globals: {
+    'ts-jest': {
+      isolatedModules: true
+    },
+  },
   preset: 'ts-jest',
   testEnvironment: 'node',
+  maxWorkers: 4,
+  transform: {
+    "^.+\\.(t|j)sx?$": "@swc/jest"
+  },
   testMatch: [
     '<rootDir>/src/**/__tests__/**/*.[jt]s?(x)',
     '<rootDir>/src/**/?(*.)+(spec|test).[jt]s?(x)',

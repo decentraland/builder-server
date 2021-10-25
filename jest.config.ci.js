2bcef6379d593d711c8d254789560b3aae73da6c
Nicolás Santángelo <santangelonicolas@gmail.com>
2021-10-25 11:31:10 -0300

    chore: commit new config

---
+++ jest.config.ci.js
@@ -0,0 +1,10 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  maxWorkers: 4,
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.[jt]s?(x)',
+    '<rootDir>/src/**/?(*.)+(spec|test).[jt]s?(x)',
+  ],
+}

0c505be44a717767099b0400e5619258637f5cff
Lautaro Petaccio <lausuper@gmail.com>
2021-10-25 17:11:49 -0300

    chore: Add ownable check of TP collections

--- src/Ownable/Ownable.ts
+++ src/Ownable/Ownable.ts
@@ -1,3 +1,8 @@
+import { CollectionAttributes } from '../Collection'
+import { Collection } from '../Collection/Collection.model'
+import { getThirdPartyCollectionURN } from '../Collection/utils'
+import { isManager } from '../ethereum/api/tpw'
+import { Item } from '../Item/Item.model'
 import { OwnableModel } from './Ownable.types'
 
 export class Ownable {
@@ -7,11 +12,35 @@ export class Ownable {
     this.Model = Model
   }
 
-  async isOwnedBy(id: string, ethAddress: string) {
-    return (await this.Model.count({ id, eth_address: ethAddress })) > 0
+  async isOwnedBy(id: string, ethAddress: string): Promise<boolean> {
+    if (!(this.Model instanceof Collection) && !(this.Model instanceof Item)) {
+      return (await this.Model.count({ id, eth_address: ethAddress })) > 0
+    }
+    let collection: CollectionAttributes | undefined
+
+    // Collections and items must be treated differently
+    if (this.Model instanceof Collection) {
+      collection = await Collection.findOne<CollectionAttributes>(id)
+    } else {
+      try {
+        collection = await Collection.findByOwnerOfItem(id)
+      } catch (_) {
+        return false
+      }
+    }
+
+    if (collection && collection.urn_suffix) {
+      return isManager(
+        getThirdPartyCollectionURN(collection.urn_suffix),
+        ethAddress
+      )
+    } else if (collection) {
+      return collection.eth_address === ethAddress
+    }
+    return false
   }
 
-  async canUpsert(id: string, ethAddress: string) {
+  async canUpsert(id: string, ethAddress: string): Promise<boolean> {
     const [count, isOwner] = await Promise.all([
       this.Model.count({ id }),
       this.isOwnedBy(id, ethAddress),

85d1aba1386bc815a18b4039b04333a4aeef6ae8
Lautaro Petaccio <lausuper@gmail.com>
2021-11-02 14:13:44 -0300

    chore: Restart server

--- src/server.ts
+++ src/server.ts
@@ -74,7 +74,7 @@ if (require.main === module) {
 }
 
 async function startServer() {
-  console.log('Connecting to the DB')
+  console.log('Connecting to the DB ')
   await db.connect()
   return app.listen(SERVER_PORT)
 }

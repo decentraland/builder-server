dcd94c1b486602d6abd6425c0d3ddbf01eac0470
Juanma Hidalgo <juanma06@gmail.com>
2022-02-15 18:33:25 +0100

    feat: Add DISTINCT ON itemCurations query

--- src/Curation/ItemCuration/ItemCuration.model.ts
+++ src/Curation/ItemCuration/ItemCuration.model.ts
@@ -11,9 +11,10 @@ export class ItemCuration extends Model<ItemCurationAttributes> {
   static async findByCollectionId(collectionId: string) {
     // prettier-ignore
     return this.query<ItemCurationAttributes>(SQL`
-    SELECT ic.*
+    SELECT DISTINCT on (i.id) ic.*
       FROM ${raw(this.tableName)} ic
-      INNER JOIN ${raw(Item.tableName)} i ON i.id = ic.item_id AND i.collection_id = ${collectionId}`)
+      INNER JOIN ${raw(Item.tableName)} i ON i.id = ic.item_id AND i.collection_id = ${collectionId} 
+      ORDER BY i.id, ic.created_at DESC`)
   }
 
   static async getItemCurationCountByThirdPartyId(thirdPartyId: string) {

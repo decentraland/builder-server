e45e2886f10d54d1bc70736e8dec6d370cb43824
Juanma Hidalgo <juanma06@gmail.com>
2022-03-04 12:49:52 -0500

    feat: Adds qty & salt to cheque column

--- migrations/1646234502768_add-cheque-signature-column.ts
+++ migrations/1646234502768_add-cheque-signature-column.ts
@@ -3,20 +3,23 @@ import { MigrationBuilder, ColumnDefinitions } from 'node-pg-migrate'
 import { SlotUsageCheque } from '../src/SlotUsageCheque'
 
 const tableName = SlotUsageCheque.tableName
-const newColumnName = 'signature'
 const oldColumnName = 'signedMessage'
+
+const columns = {
+  signature: { type: 'TEXT', notNull: true },
+  salt: { type: 'VARCHAR(32)', notNull: true },
+  qty: { type: 'INT', notNull: true },
+}
 
 export const shorthands: ColumnDefinitions | undefined = undefined
 
 export async function up(pgm: MigrationBuilder): Promise<void> {
   pgm.dropColumn(tableName, oldColumnName)
-  pgm.addColumn(tableName, {
-    [newColumnName]: { type: 'TEXT', notNull: true },
-  })
+  pgm.addColumn(tableName, columns)
 }
 
 export async function down(pgm: MigrationBuilder): Promise<void> {
-  pgm.dropColumn(tableName, newColumnName)
+  pgm.dropColumn(tableName, columns)
   pgm.addColumn(tableName, {
     [oldColumnName]: { type: 'TEXT', notNull: true },
   })

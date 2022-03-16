7fabed66bc12bbcc31aa350a9013c671c630e65a
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2022-03-16 14:25:18 -0300

    chore: Add tests for getting a single nft

--- src/NFT/NFT.router.spec.ts
+++ src/NFT/NFT.router.spec.ts
@@ -2,12 +2,15 @@ import supertest from 'supertest'
 import { STATUS_CODES } from '../common/HTTPError'
 import { app } from '../server'
 import { NFTService } from './NFT.service'
+import { NFT } from './NFT.types'
 
 jest.mock('./NFT.service')
 
 const mockNFTService = NFTService as jest.MockedClass<typeof NFTService>
 
 const server = supertest(app.getApp())
+
+const mockAddress = '0x6D7227d6F36FC997D53B4646132b3B55D751cc7c'
 
 afterEach(() => {
   jest.clearAllMocks()
@@ -135,7 +138,7 @@ describe('when getting nfts', () => {
   })
 
   describe('when the nft service rejects', () => {
-    it('should faild with a server error', async () => {
+    it('should fail with a server error', async () => {
       mockNFTService.prototype.getNFTs.mockReset()
       mockNFTService.prototype.getNFTs.mockRejectedValueOnce('Rejected!')
 
@@ -167,17 +170,94 @@ describe('when getting nfts', () => {
   describe('when all query params are provided', () => {
     it('should call the nft service with them', async () => {
       await server
-        .get(
-          '/v1/nfts?owner=0x6D7227d6F36FC997D53B4646132b3B55D751cc7c&first=10&skip=10&cursor=cursor'
-        )
+        .get(`/v1/nfts?owner=${mockAddress}&first=10&skip=10&cursor=cursor`)
         .expect(STATUS_CODES.ok)
 
       expect(mockNFTService.prototype.getNFTs).toHaveBeenCalledWith({
         cursor: 'cursor',
         first: 10,
-        owner: '0x6D7227d6F36FC997D53B4646132b3B55D751cc7c',
+        owner: mockAddress,
         skip: 10,
       })
+    })
+  })
+})
+
+describe('when getting a single nft', () => {
+  describe('when contractAddress path param does not have a valid etherum address pattern', () => {
+    it('should fail with a bad request error', async () => {
+      const response = await server
+        .get('/v1/nfts/asd/123')
+        .expect(STATUS_CODES.badRequest)
+
+      expect(response.body).toEqual({
+        data: {
+          dataPath: '/contractAddress',
+        },
+        error: 'should match pattern "^0x[a-fA-F0-9]{40}$"',
+        ok: false,
+      })
+    })
+  })
+
+  describe('when the nft service rejects', () => {
+    it('should fail with a server error', async () => {
+      mockNFTService.prototype.getNFT.mockRejectedValueOnce('Rejected!')
+
+      const response = await server
+        .get(`/v1/nfts/${mockAddress}/123`)
+        .expect(STATUS_CODES.error)
+
+      expect(response.body).toEqual({
+        data: {
+          contractAddress: mockAddress,
+          tokenId: '123',
+        },
+        error: 'Failed to fetch NFT from external sources',
+        ok: false,
+      })
+    })
+  })
+
+  describe('when the nft service resolves undefined', () => {
+    it('should fail with a not found error', async () => {
+      mockNFTService.prototype.getNFT.mockResolvedValueOnce(undefined)
+
+      const response = await server
+        .get(`/v1/nfts/${mockAddress}/123`)
+        .expect(STATUS_CODES.notFound)
+
+      expect(response.body).toEqual({
+        data: {
+          contractAddress: mockAddress,
+          tokenId: '123',
+        },
+        error: 'NFT not found',
+        ok: false,
+      })
+    })
+  })
+
+  it('should return an nft', async () => {
+    const nft: NFT = {
+      name: 'name',
+      thumbnail: 'thumbnail',
+      tokenId: 'tokenId',
+      contract: {
+        address: 'address',
+        name: 'name',
+      },
+    } as NFT
+
+    mockNFTService.prototype.getNFT.mockResolvedValueOnce(nft)
+
+    const response = await server
+      .get(`/v1/nfts/${mockAddress}/123`)
+      .expect(STATUS_CODES.ok)
+
+    expect(response.body).toEqual({
+      data: nft,
+      ok: true,
     })
   })
 })

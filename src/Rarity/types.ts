dbe0725857082b89a5e8f6b7efe3c063e5ea9d65
Nando <24811313+fzavalia@users.noreply.github.com>
2022-05-09 11:14:30 -0300

    chore: Add comment to Rarity type

--- src/Rarity/types.ts
+++ src/Rarity/types.ts
@@ -4,6 +4,21 @@ export enum Currency {
   USD = 'USD',
 }
 
+/**
+ * When using the RaritiesWithOracle contract:
+ * 
+ * - "price" is the price in MANA converted from "originalCurrency" of the rarity.
+ * - "originalPrice" is the price in "originalCurrency" for the rarity.
+ * - "originalCurrency" is the currency from which the price has been converted from.
+ * 
+ * NOTE: In this version, the only existing currency that is converted to MANA is "USD"
+ * 
+ * When using the original Rarities contract:
+ * 
+ * - "price" is the price in MANA for the rarity without any conversion, returned as is from the graph.
+ * - "originalPrice" is always undefined.
+ * - "originalCurrency" is always undefined.
+ */
 export type Rarity = RarityFragment & {
   originalPrice?: string
   originalCurrency?: Currency

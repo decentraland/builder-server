16182a9c4f4a702d9fd9d2eb46a5f97f1d132690
Juanma Hidalgo <juanma06@gmail.com>
2022-08-22 16:18:06 -0300

    feat: update test

--- src/Item/hashes.spec.ts
+++ src/Item/hashes.spec.ts
@@ -115,7 +115,7 @@ describe('when calculating the hashes of a standard emote item', () => {
     return expect(
       calculateItemContentHash(dbItem, dbCollection)
     ).resolves.toEqual(
-      'bafkreih2ln2rws646fyk56xwmysgk27cdwqnm6wa343nanlpiamf44vpfa'
+      'bafkreicfhee6f7tovvz5w3xd62ki7e6fdsvo7nxnd2krdjaotuxt3uuyvu'
     )
   })
 })

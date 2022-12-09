2e4e02243b4196f14d2572757aa6d41107469127
Gabriel Diaz <gabrieldiaz31@gmail.com>
2022-12-09 14:37:53 -0300

    feat: Update logic to move allowed items between collections

--- src/Item/Item.service.ts
+++ src/Item/Item.service.ts
@@ -72,8 +72,14 @@ export class ItemService {
     }
 
     if (dbItem) {
-      // Moving items between collections is forbidden
-      this.checkItemIsMovedToAnotherCollection(item, dbItem)
+      // Moving items between published collections is forbidden
+      if (
+        this.checkItemIsMovedToAnotherCollection(item, dbItem) &&
+        (!(await this.checkItemCollectionIsValidToMove(item.collection_id)) ||
+          !(await this.checkItemCollectionIsValidToMove(dbItem.collection_id)))
+      ) {
+        throw new ItemCantBeMovedFromCollectionError(item.id)
+      }
     }
 
     const collectionId: string | null =
@@ -340,20 +346,40 @@ export class ItemService {
     return { item, collection }
   }
 
+  private async checkItemCollectionIsValidToMove(
+    collectionId: string | null
+  ): Promise<boolean> {
+    if (collectionId) {
+      const collection = await this.collectionService.getDBCollection(
+        collectionId
+      )
+
+      if (isTPCollection(collection)) {
+        return false
+      }
+
+      const isCollectionPublished =
+        collection.contract_address &&
+        (await this.collectionService.isDCLPublished(
+          collection.contract_address
+        ))
+      return isCollectionPublished === false
+    }
+
+    return false
+  }
+
   private checkItemIsMovedToAnotherCollection(
     itemToUpsert: FullItem,
     dbItem: ItemAttributes
-  ): void {
+  ): boolean {
     const areBothCollectionIdsDefined =
-      itemToUpsert.collection_id && dbItem.collection_id
+      !!itemToUpsert.collection_id && !!dbItem.collection_id
 
-    const isItemCollectionBeingChanged =
+    return (
       areBothCollectionIdsDefined &&
       itemToUpsert.collection_id !== dbItem.collection_id
-
-    if (isItemCollectionBeingChanged) {
-      throw new ItemCantBeMovedFromCollectionError(itemToUpsert.id)
-    }
+    )
   }
 
   private async deleteDCLItem(dbItem: ItemAttributes): Promise<void> {

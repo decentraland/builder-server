b3759498f577f44fa51e1b4a2343d3a4f4d88c13
Melisa Anabella Rossi <melisa.rossi@decentraland.org>
2023-06-08 18:02:08 -0300

    fix linter

--- src/Item/Item.service.ts
+++ src/Item/Item.service.ts
@@ -456,7 +456,7 @@ export class ItemService {
       ))
 
     const isCollectionOwner =
-    dbCollection && dbCollection.eth_address.toLowerCase() === eth_address
+      dbCollection && dbCollection.eth_address.toLowerCase() === eth_address
 
     const isManager =
       isDbCollectionPublished &&

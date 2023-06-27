1796446b8e66d4289ae5e8380a1f7a902919b5b0
Melisa Anabella Rossi <melisa.rossi@decentraland.org>
2023-06-27 14:51:12 -0300

    fix: remove template fields to upsert pool (#666)

--- src/Pool/Pool.router.ts
+++ src/Pool/Pool.router.ts
@@ -131,6 +131,9 @@ export class PoolRouter extends Router {
       is_public,
       is_deleted,
       creation_coords,
+      is_template,
+      video,
+      template_status,
       ...upsertPool
     } = (project || {}) as ProjectAttributes
 

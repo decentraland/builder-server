c6e6432c600305c2e4e5f03920520a48b07aee37
Juanma Hidalgo <juanma06@gmail.com>
2023-09-07 13:21:34 +0200

    feat: introduce the /newsletter endpoint to subscribe users to dcl newsletter

---
+++ src/Newsletter/Newsletter.model.ts
@@ -0,0 +1,41 @@
+import nodefetch from 'node-fetch'
+import { env } from 'decentraland-commons'
+import { SubscriptionResponse } from './Newsletter.types'
+
+const NEWSLETTER_SERVICE_URL = env.get('NEWSLETTER_SERVICE_URL', '')
+const NEWSLETTER_PUBLICATION_ID = env.get('NEWSLETTER_PUBLICATION_ID', '')
+const NEWSLETTER_SERVICE_API_KEY = env.get('NEWSLETTER_SERVICE_API_KEY', '')
+
+export namespace Newsletter {
+  export async function subscribe(
+    email: string
+  ): Promise<SubscriptionResponse | null> {
+    try {
+      const options = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+          Authorization: `Bearer ${NEWSLETTER_SERVICE_API_KEY}`,
+        },
+        body: JSON.stringify({
+          email,
+          reactivate_existing: true,
+          send_welcome_email: false,
+          utm_source: 'Builder',
+          utm_campaign: 'Builder',
+          utm_medium: 'organic',
+        }),
+      }
+      const response = await nodefetch(
+        `${NEWSLETTER_SERVICE_URL}/publications/${NEWSLETTER_PUBLICATION_ID}/subscriptions`,
+        options
+      )
+
+      return response.json()
+    } catch (error) {
+      console.error(error)
+      return null
+    }
+  }
+}

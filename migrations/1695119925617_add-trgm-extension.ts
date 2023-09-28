e0097f4cbed2f5937e318c71b423648f987e7778
Juanma Hidalgo <juanma06@gmail.com>
2023-09-28 14:00:28 +0200

    fix: remove the CREATE trgm extension from migration (#712)

--- migrations/1695119925617_add-trgm-extension.ts
+++ migrations/1695119925617_add-trgm-extension.ts
@@ -4,10 +4,5 @@ import { MigrationBuilder, ColumnDefinitions } from 'node-pg-migrate'
 export const shorthands: ColumnDefinitions | undefined = undefined
 
 export async function up(pgm: MigrationBuilder): Promise<void> {
-  pgm.addExtension('pg_trgm', { ifNotExists: true })
   pgm.sql('SET pg_trgm.similarity_threshold = 0.5')
-}
-
-export async function down(pgm: MigrationBuilder): Promise<void> {
-  pgm.dropExtension('pg_trgm')
 }

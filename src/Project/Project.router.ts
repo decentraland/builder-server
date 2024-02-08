99aa6aa9b0d7b5cfd32f48a8246bbc10c399fefc
Gon Pombo <gonzalo@decentraland.org>
2024-02-08 13:48:18 -0300

    Add realm name to the /about endpoint.  (#729)
    * Add realm name to the /about endpoint. Also set fixed skybox and minimap in false
    
    * change name
    
    * fix name

--- src/Project/Project.router.ts
+++ src/Project/Project.router.ts
@@ -360,6 +360,13 @@ export class ProjectRouter extends Router {
         scenesUrn: [
           `urn:decentraland:entity:${ENTITY_HASH}?=&baseUrl=${BUILDER_SERVER_URL}/v1/projects/${projectId}/contents/`,
         ],
+        minimap: {
+          enabled: false
+        },
+        skybox: {
+          fixedHour: 36000
+        },
+        realmName: `web-editor-${projectId.split('-').slice(-1)}`
       },
       content: {
         healthy: true,

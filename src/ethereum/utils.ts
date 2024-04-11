e0b26d377d984bdbc9b90bf0ac881b15803e3889
Gabriel Diaz <gabrieldiaz31@gmail.com>
2024-04-11 16:55:07 -0300

    feat: Update mapped chain id to use amoy

--- src/ethereum/utils.ts
+++ src/ethereum/utils.ts
@@ -8,7 +8,7 @@ export function getChainName() {
 
 export function getMappedChainIdForCurrentChainName():
   | ChainId.MATIC_MAINNET
-  | ChainId.MATIC_MUMBAI {
+  | ChainId.MATIC_AMOY {
   const chainName = getChainName()
 
   switch (chainName) {
@@ -16,7 +16,7 @@ export function getMappedChainIdForCurrentChainName():
       return ChainId.MATIC_MAINNET
     case ChainName.ETHEREUM_GOERLI:
     case ChainName.ETHEREUM_SEPOLIA:
-      return ChainId.MATIC_MUMBAI
+      return ChainId.MATIC_AMOY
     default:
       throw new Error(
         `The chain name ${chainName} doesn't have a chain id to map to`
@@ -42,6 +42,8 @@ export function getFactoryCollectionCodeHash() {
       '0x5a1d707e8f0be7be88213a8216231468689b96dcd4abed0931276f4886a87beb',
     [ChainId.MATIC_MUMBAI]:
       '0x7917e9ddbe5e0fd8de84efee3e8089ca7878af7a6aa1a62b4d0b6160821d4de8',
+    [ChainId.MATIC_AMOY]:
+      '0xc5bd2e95bc8bf4499c511b9b40aef6298cda5a6d154869db199c160e29672930'
   }
 
   const chainId = getMappedChainIdForCurrentChainName()

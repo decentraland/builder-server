7e72c1604663dc59b25d96bcd80e099e8caa02db
Gabriel Diaz <gabrieldiaz31@gmail.com>
2024-04-18 09:42:43 -0300

    fix: Update TP mock data

--- spec/mocks/cheque.ts
+++ spec/mocks/cheque.ts
@@ -1,6 +1,6 @@
 export const mockedCheque = {
   signature:
-    '0xda1398329aff61ce133d4204c0ebc656b07e0629c280d6f60121874285ac3bbc56e48b9e7211296be9f209d646e723f44a96c82f0e6bd52ee6edf74cc44ee9d71c', // signatured generated using the wallet from fakePrivateKey
+    '0x784093055673ce56a4a4093a7ec5e3a9594f101682774d980da777d53913760f189b7f1a8d9b4a5123ac113f3407cfcd418868545a455a51fa431223be8fa1931b', // signatured generated using the wallet from fakePrivateKey
   qty: 3,
-  salt: '0xb899ffa17ccea3e2ae5fd36236df56c3bf908a435ad101ed7fa6ec8a9631e253',
+  salt: '0xdf421c292bafb6f4f8a50fddbe03a4205ef289e2d9377eda4560cf1f94071d14',
 }

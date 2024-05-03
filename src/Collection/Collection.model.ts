52b612f6200f85472c83b46ec2eb916db57657f7
Melisa Anabella Rossi <melisa.rossi@decentraland.org>
2024-05-03 16:22:18 -0300

    fix: collection search lowercase (#747)

--- src/Collection/Collection.model.ts
+++ src/Collection/Collection.model.ts
@@ -105,12 +105,13 @@ export class Collection extends Model<CollectionAttributes> {
     ) {
       return SQL``
     }
+    const search = `%${q}%`
     const isStandard = SQL`collections.third_party_id is NULL AND collections.urn_suffix is NULL`
     const isThirdParty = SQL`collections.third_party_id is NOT NULL AND collections.urn_suffix is NOT NULL`
     const isInRemoteIds = SQL`collections.contract_address = ANY(${remoteIds})`
     const sameStatusAndInTheBlockchain = SQL`(collection_curations.status = ${status} AND (${isInRemoteIds} OR (${isThirdParty})))`
     const conditions = [
-      q ? SQL`collections.name % ${q} ` : undefined,
+      q ? SQL`collections.name ILIKE ${search} ` : undefined,
       assignee ? SQL`collection_curations.assignee = ${assignee}` : undefined,
       address
         ? thirdPartyIds?.length

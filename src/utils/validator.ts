98afd79da5e84cb1836c11cf4b631ed50a1a75b8
LautaroPetaccio <lausuper@gmail.com>
2024-07-24 19:04:40 -0300

    fix: Adds comment

--- src/utils/validator.ts
+++ src/utils/validator.ts
@@ -2,6 +2,7 @@ import Ajv from 'ajv'
 import { Mappings, RangeMapping } from '@dcl/schemas'
 import addFormats from 'ajv-formats'
 
+// Patch the mappings validator to accept mappings that are null
 const mappingsValidator = (...args: any[]): Promise<any> | boolean =>
   args[1] && Mappings._isMappingsValid.validate
     ? Mappings._isMappingsValid.validate.apply(

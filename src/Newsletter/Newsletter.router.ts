d4ab25d7d4a0ecececf1e4526c29d928d343a495
Gabriel Diaz <gabrieldiaz31@gmail.com>
2026-03-04 14:56:29 -0300

    fix: Newsletter endpoints

--- src/Newsletter/Newsletter.router.ts
+++ src/Newsletter/Newsletter.router.ts
@@ -10,37 +10,16 @@ export class NewsletterRouter extends Router {
      * CORS for the OPTIONS header
      */
     this.router.options('/newsletter', withCors)
-    this.router.options('/newsletter/:subscriptionId', withCors)
 
     this.router.post(
       '/newsletter',
       withCors,
       server.handleRequest(this.subscribe)
     )
-    this.router.delete(
-      '/newsletter/:subscriptionId',
-      withCors,
-      server.handleRequest(this.deleteSubscription)
-    )
-    this.router.get(
-      '/newsletter/:subscriptionId',
-      withCors,
-      server.handleRequest(this.getSubscription)
-    )
   }
 
   async subscribe(req: Request) {
     const { email, source } = req.body
-    return Newsletter.subscribe(email, source)
-  }
-
-  async deleteSubscription(req: Request) {
-    const subscriptionId = server.extractFromReq(req, 'subscriptionId')
-    return Newsletter.deleteSubscription(subscriptionId)
-  }
-
-  async getSubscription(req: Request) {
-    const subscriptionId = server.extractFromReq(req, 'subscriptionId')
-    return Newsletter.getSubscription(subscriptionId)
+    await Newsletter.subscribe(email, source)
   }
 }
